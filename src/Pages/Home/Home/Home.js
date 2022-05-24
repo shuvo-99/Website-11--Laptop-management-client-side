f574f32473d791f38d1e0174142ee4ea033240fd
shuvo-99 <anontoshuvo99@gmail.com>
2022-05-24 16:30:29 +0600

    added banner

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Banner from "../Banner/Banner";
 import Items from "../Items/Items";
 const Home = () => {
   return (
     <div>
       <h2>this is home</h2>
+      <Banner></Banner>
+      <br />
       <Items></Items>
     </div>
   );
